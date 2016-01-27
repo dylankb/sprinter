17eb7c99553f0b02bf0bbd9ea651778ae9e4fb13
Dylan Barnard <dylan.barnard@gmail.com>
2016-01-26 21:56:57 -0800

    Improve UI, implement data validation, change page content, fix ability to start over

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ use Rack::Session::Cookie, :key => 'rack.session',
                            :path => '/',
                            :secret => 'barmaged0n'
 
-SPRINT_DAYS = 14
+SPRINT_DAYS = 14.0
 MINUTES_IN_HOUR = 60
 
 helpers do
@@ -17,6 +17,19 @@ helpers do
   def format_time_in_hours(time)
     time.to_f % 1 == 0 ? time.to_i / 60 : time.to_f / 60
   end
+
+  def round_all_fractions_up(days)
+    days.to_f % 1 == 0 ? days : days.to_i + 1
+  end
+
+  def calculate_total_time(projects)
+    total_time = 0
+    projects.each do |key,value|
+      total_time += value['time']
+    end
+    total_time
+  end
+
 end
 
 get '/' do
@@ -33,15 +46,16 @@ end
 
 post '/new_user' do
   session[:projects] = nil
+  session[:time_available] = nil
+  session[:show_results_button] = false
 
   if params[:username].empty?
-    @error = "Please enter something here"
+    @error = "Please enter your name."
     halt erb(:get_name)
   else
     session[:username] = params[:username]
     redirect '/project_entry'
-  end
-
+  end 
 end
 
 get '/project_entry' do
@@ -49,6 +63,16 @@ get '/project_entry' do
 end
 
 post '/add_project' do
+
+  if params[:project_name].empty?
+    @error= 'Please add a name for your project.'
+    halt erb(:project_entry)
+  end
+
+  if !(/^[0-9]*\.?[0-9]+/ =~ params[:project_minutes])
+    @error= 'Please enter a valid number for your estimated project time.'
+    halt erb(:project_entry)
+  end
   
   project = {
     params[:project_name] => { 
@@ -56,17 +80,13 @@ post '/add_project' do
       'days'=> 0 } 
     }
 
-  project[params[:project_name]]['time'] = project[params[:project_name]]['time'] * 60
-  binding.pry
-
+  project[params[:project_name]]['time'] *= MINUTES_IN_HOUR
 
   if session[:projects]
     session[:projects].merge!(project)
   else
     session[:projects] = project 
   end
-
-
 
   session[:show_availability_button] = true
   redirect '/project_list'
@@ -81,8 +101,15 @@ get '/availability' do
 end
 
 post '/availability' do
+  time_in_minutes = params[:time_available].to_f * MINUTES_IN_HOUR * SPRINT_DAYS
+
+  if time_in_minutes < calculate_total_time(session[:projects])
+    @error = "Hmm, if you only spend #{params[:time_available]} hours a day it doesn't look like you'll be able to finish all your tasks by the end of the two weeks. Try entering a higher number."
+    halt erb(:get_availability)
+  end   
+
   session[:hours_available] = params[:time_available]
-  session[:time_available] = params[:time_available].to_f * MINUTES_IN_HOUR * SPRINT_DAYS
+  session[:time_available] = time_in_minutes
   session[:show_results_button] = true
   session[:show_availability_button] = false
 
@@ -91,7 +118,6 @@ end
 
 post '/calculate_sprint' do
 
-  #binding.pry
   daily_projects_times = []
   days_to_complete_projects = 0
   projects_updated = session[:projects].dup
@@ -124,9 +150,9 @@ post '/calculate_sprint' do
     end
   end
 
-  session[:days_to_complete_projects] = days_to_complete_projects.to_i
+  binding.pry
+  session[:days_to_complete_projects] = round_all_fractions_up(days_to_complete_projects)
   session[:projects_updated] = projects_updated
-  #binding.pry
   session[:today] = Time.new.to_date
   
   redirect '/project_list_results'
